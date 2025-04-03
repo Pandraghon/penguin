bb52baabc671e99d724f43968f27e13ba0b2cf82
Pandraghon <pandraghon@outlook.com>
2025-04-03 11:58:18 +0000

    Update gh-pages 9f77c4a0b2f9076097f636713ff61c0e247ab160

--- js/data/en.js
+++ js/data/en.js
@@ -123,7 +123,7 @@ const data = {
                 data: [{
                     content: `Curious and passionate, I regularly keep up to date with the latest trends in software and web programming through an active technology watch.`,
                 }, {
-                    content: `Rigorous, organized and conscientious, I easily adapt to new situations and environments. Being customer oriented, I am able to transform client requirements into valuable delivrables.`,
+                    content: `Rigorous, organized and conscientious, I easily adapt to new situations and environments. Being customer oriented, I am able to transform client requirements into valuable deliverables.`,
                 }, {
                     content: `Serious and diligent, I make it a point of honor to only produce quality work.`,
                 }]
@@ -179,11 +179,14 @@ const data = {
                     title: 'Software Development Engineer & Solution Architect',
                     location: 'Syentys, France',
                     description: `<ul>
-                    <li>Odoo ERP integration for various customers, including dental equipment manufacturers, butchers, financial journalists, pizza vending machine manufacturers, etc.</li>
-                    <li>Project Manager: planning and follow-ups with the team and the customer.</li>
+                    <li>Odoo ERP integration and maintenance for various customers, including dental equipment manufacturers, butchers, financial journalists, pizza vending machine manufacturers, etc.</li>
+                    <li>Customers' data migration from their previous system to Odoo ERP.</li>
+                    <li>Odoo Connectors with other APIs such as Prestashop, etc.</li>
+                    <li>Training with customer</li>
+                    <li>Project Manager: planning (Agile) / team management, test definition and monitoring (Installation Qualification, Operational Qualification, Performance Qualification) and direct interface with the customer.</li>
                     <li>Solution Architect: analysing and challenging customer's requirements with the default features of the solution, translating business requirements into technical tasks for optimal implementation.</li>
-                    <li>Technical Advisor: Odoo functional and technical expertise, Docker and Linux knowledge, code reviews.</li>
-                    <li>Improvement of development and deployment procedures: teaching developers to use debbuging tools, installation and deployement procedures on Linux hosts.</li>
+                    <li>Technical Advisor: Odoo functional and technical expertise, Docker and Linux technical reference, organization of code reviews with the developers.</li>
+                    <li>Improvement of development and deployment procedures: teaching developers to use debbuging tools, installation and deployment procedures on Linux hosts.</li>
                     <li>R&D for C#-based solution for building security system.</li>
                     </ul>`,
                     technologies: `ERP, Python, JS, HTML, Java, Talend, CI/CD, Github, AWS, Azure`,
@@ -193,7 +196,7 @@ const data = {
                     icon_text: 'Internship',
                     start: '02/2017',
                     end: '07/2017',
-                    title: 'Master Internship',
+                    title: 'Developer (Master Internship)',
                     location: 'Syentys, France',
                     description: `<ul>
                     <li>Feasibility study of an embeded hardware and software version of Odoo in craftmanship's factories and vehicles.</li>
@@ -215,7 +218,7 @@ const data = {
                     icon_text: 'Internship',
                     start: '05/2015',
                     end: '09/2015',
-                    title: 'Bachelor Internship',
+                    title: 'Developer (Bachelor Internship)',
                     location: 'META4, France',
                     description: 'Development of an internal workflow-based package management and delivery tool.',
                     technologies: `Access, PHP, JS, HTML`,
@@ -246,8 +249,8 @@ const data = {
                 data: [{
                     id: 'gw2achievements',
                     name: 'GW2Achievements',
-                    for: 'Personnal project',
-                    description: 'Image generator (GW2 success) based on user-selectable parameters',
+                    for: 'Personal project',
+                    description: 'Image generator (GW2 achievements) based on user-selectable parameters',
                     technologies: ['NodeJS', 'Canvas', 'Docker'],
                     git: 'https://github.com/gw2ocs/gw2achievements',
                     url: 'https://gw2achievements.com'
@@ -255,8 +258,8 @@ const data = {
                 {
                     id: 'gw2trivia',
                     name: 'GW2Trivia',
-                    for: 'Personnal project',
-                    description: 'Website for Questions pour un Quaggan, a general knowledge quiz on the GW2 universe',
+                    for: 'Personal project',
+                    description: 'Website for "Questions pour un Quaggan", a general knowledge quiz on the GW2 universe',
                     technologies: ['NodeJS', 'PostgreSQL', 'plpgsql', 'Postgraphile', 'QraphQL', 'JS', 'API', 'SEO', 'HTML', 'CSS', 'Docker'],
                     git: 'https://github.com/gw2ocs/gw2trivia',
                     url: 'https://gw2trivia.com'
@@ -264,23 +267,23 @@ const data = {
                 {
                     id: 'ogden',
                     name: 'Ogden',
-                    for: 'Personnal project',
-                    description: 'Discord Bot for Questions pour un Quaggan',
+                    for: 'Personal project',
+                    description: 'Discord Bot for "Questions pour un Quaggan": chatbot in a GW2 community Discord server, asking question every hour.',
                     technologies: ['NodeJS', 'PostgreSQL', 'QraphQL', 'JS', 'API', 'Discord', 'Docker'],
                     git: 'https://github.com/gw2ocs/ogden'
                 },
                 {
                     id: 'triagui',
                     name: 'TriaGUI',
-                    for: 'M1 MIAGE project',
-                    description: 'Tool for constructing and displaying Delaunay triangulation over several degrees.',
-                    technologies: ['C++', 'Qt', 'interface', 'dessin', 'geométrie'],
+                    for: 'MIAGE Master project',
+                    description: 'Tool for building and displaying Delaunay triangulation.',
+                    technologies: ['C++', 'Qt', 'interface', 'drawing', 'geometry'],
                     git: 'https://github.com/Pandraghon/TriaGUI'
                 },
                 {
                     id: 'gw2icon',
                     name: 'GW2Icon',
-                    for: 'Personnal project',
+                    for: 'Personal project',
                     description: 'Plugin for including GW2 game icons in web pages, using pure CSS.',
                     technologies: ['CSS', 'Sass', 'NodeJS', 'HTML', 'gulp.js', 'TravisCI'],
                     git: 'https://github.com/gw2ocs/gw2icon',
@@ -289,14 +292,14 @@ const data = {
                 {
                     id: 'meta4',
                     name: 'SaaSTools',
-                    for: 'L3 MIAGE Internship at META4',
+                    for: 'MIAGE Bachelor Internship at META4',
                     description: 'Internal tool to manage package delivery via a workflow.',
                     technologies: ['Access', 'PHP', 'MySQL', 'JS', 'AJAX', 'HTML', 'CSS']
                 },
                 {
                     id: 'anoname',
                     name: 'Anoname',
-                    for: 'Personnal project',
+                    for: 'Personal project',
                     description: 'Name generator based on the Markov chain principle.',
                     technologies: ['C++', 'Qt'],
                     git: 'https://github.com/Pandraghon/anoname'
@@ -327,7 +330,7 @@ const data = {
                     id: 'amongus',
                     name: 'BetterSabotage & UnknownImpostor',
                     for: 'Personnal projects',
-                    description: 'Mods for the game AmongUs, injecting additional features.',
+                    description: 'Code development (mods) for the game AmongUs, injecting additional features.',
                     technologies: ['C#', 'Modding'],
                     git: 'https://github.com/Pandraghon/BetterSabotage',
                 }]
